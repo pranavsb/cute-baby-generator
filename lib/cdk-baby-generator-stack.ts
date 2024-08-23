f11ee9a2ed3af1f1a6106e21c74bf7823ad48333
Pranav Bijapur <bijapurpranav@gmail.com>
2024-08-22 22:39:14 -0700

    chore: cleanup

--- lib/cdk-baby-generator-stack.ts
+++ lib/cdk-baby-generator-stack.ts
@@ -4,14 +4,11 @@ import { HttpLambdaIntegration } from 'aws-cdk-lib/aws-apigatewayv2-integrations
 import { Function, Runtime, Code } from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
 import { join } from 'path';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkBabyGeneratorStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // TODO use lambda proxy integration?
-    //https://cdkworkshop.com/20-typescript/30-hello-cdk/400-apigw.html
     const cuteBabyApi = new HttpApi(this, 'CuteBabyGeneratorApi', {
       apiName: 'CuteBabyGeneratorApi',
       corsPreflight: {
@@ -19,8 +16,7 @@ export class CdkBabyGeneratorStack extends cdk.Stack {
       }
     });
 
-    const cuteBabyImageLambda =  
-    new Function(this, 'CuteBabyImageLambda', {
+    const cuteBabyImageLambda = new Function(this, 'CuteBabyImageLambda', {
       code: Code.fromAsset(join(__dirname, '..', 'dist', 'index.zip')),
       handler: 'index.handler',
       runtime: Runtime.NODEJS_20_X,
@@ -33,9 +29,5 @@ export class CdkBabyGeneratorStack extends cdk.Stack {
       methods: [ HttpMethod.GET ],
       integration: lambdaIntegration
     });
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkBabyGeneratorQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
   }
 }
